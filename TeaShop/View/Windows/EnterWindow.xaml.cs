229f9d7c2b5c70d13ea3f0987b3cb24b43dbab57
Lesya-P-Hub <lesya.petrova18@gmail.com>
2024-02-18 22:56:40 +0300

    Добавьте файлы проекта.

---
+++ TeaShop/View/Windows/EnterWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace TeaShop.View.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для EnterWindow.xaml
+    /// </summary>
+    public partial class EnterWindow : System.Windows.Window
+    {
+        public EnterWindow()
+        {
+            InitializeComponent();
+            Loaded += EnterWindow_Loaded;
+        }
+
+        private void EnterWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.Activate();
+        }
+    }
+}
---
+++ TeaShop/View/Windows/ShopMainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using TeaShop.AppData;
+
+namespace TeaShop.View.Window
+{
+    /// <summary>
+    /// Логика взаимодействия для ShopMainWindow.xaml
+    /// </summary>
+    public partial class ShopMainWindow : System.Windows.Window
+    {
+        public ShopMainWindow()
+        {
+            InitializeComponent();
+            FrameClass.LogoFrame = LogoFrm;
+            LogoFrm.Navigate( new View.Pages.LogoPage());
+            FrameClass.BodyFrame = BodyFrm;
+            BodyFrm.Navigate(new View.Pages.MainBodyPage());
+            FrameClass.MenuFrame = MenuFrm;
+            MenuFrm.Navigate( new View.Pages.MenuPage());
+        }
+    }
+}
